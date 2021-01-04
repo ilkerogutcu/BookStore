85a2bc9dd74a1afc4c93cfad9c73f6f5cbe40925
ilker <ilkerogtc@gmail.com>
2021-01-04 23:16:05 +0300

    added controller

---
+++ WebAPI/Controllers/BooksController.cs
@@ -0,0 +1,24 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController:ControllerBase
+    {
+        private readonly IBookService _bookService;
+
+        public BooksController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet("get/all")]
+        public IActionResult GetList()
+        {
+            return Ok(_bookService.GetAll());
+        }
+    }
+}
