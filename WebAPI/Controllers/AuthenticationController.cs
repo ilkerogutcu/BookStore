2df878265f7a506a703a37d1991d95d144df8da5
ilker <ilkerogtc@gmail.com>
2021-01-05 22:34:33 +0300

    Added register and login with Microsoft.AspNetCore.Identity

--- WebAPI/Controllers/AuthenticationController.cs
+++ WebAPI/Controllers/AuthenticationController.cs
@@ -1,18 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
-using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Business.Abstract;
-using Entities.Concrete.Authentication;
 using Entities.DTOs;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.Tokens;
 
 namespace WebAPI.Controllers
 {
@@ -20,8 +10,8 @@ namespace WebAPI.Controllers
     [ApiController]
     public class AuthenticationController : ControllerBase
     {
-
         private readonly IAuthService _authService;
+
         public AuthenticationController(IAuthService authService)
         {
             _authService = authService;
@@ -31,29 +21,24 @@ namespace WebAPI.Controllers
         [Route("register")]
         public async Task<IActionResult> Register([FromBody] RegisterRequest registerRequest)
         {
-
-            if (_authService.Register(registerRequest).Result.Succeeded)
-            {
-                return Ok(new Response
+            return _authService.Register(registerRequest).Result.Succeeded
+                ? Ok(new Response
                 {
                     Status = "Success",
                     Message = "User created successfully"
+                })
+                : StatusCode(StatusCodes.Status500InternalServerError, new Response
+                {
+                    Status = "Error",
+                    Message = "User creation failed"
                 });
-            }
-
-            return StatusCode(StatusCodes.Status500InternalServerError, new Response
-            {
-                Status = "Error",
-                Message = "User creation failed"
-            });
         }
 
         [HttpPost]
         [Route("login")]
-        public async Task<IActionResult> Login([FromBody] LoginRequest loginRequest)
+        public async Task<Task<string>> Login([FromBody] LoginRequest loginRequest)
         {
-            return null;
-
+            return _authService.Login(loginRequest);
         }
     }
 }
